0bcae1593f0d34847b69bb49774dd26a9546189d
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-07-09 02:12:42 +0300

    add php for more consistent navigation, styles and analytics

---
+++ public/_head.php
@@ -0,0 +1,10 @@
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-capable" content="yes">
+
+<link rel="stylesheet" type="text/css" href="/style3.css" />
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700&subset=latin,cyrillic' rel='stylesheet'
+	type='text/css'>
+<link
+	href='https://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700|PT+Serif+Caption|PT+Serif:400,700|Russo+One|Righteous|Marmelad|Cuprum:400,700&subset=latin,cyrillic'
+	rel='stylesheet' type='text/css'>
---
+++ public/_nav.php
@@ -0,0 +1,19 @@
+<nav>
+		<div id="logo">
+			<a href="https://gratheon.com/">
+				<img alt="logo" src="img/logo_v7w.svg" class="desktop" />
+				<img alt="logo" src="img/logo_v7.svg" class="mobile" />
+			</a>
+		</div>
+		<div class="menu" style="flex-grow:1">
+			<a href="Gratheon fdac3638d55542ddb073ae90537cd5b5.html">About</a>
+			<a href="Problems we are solving 15a899e8bf10455c9ef903c6e269af2c.html">Problems</a>
+			<a href="Products 58e0ca42932f483aa6654f96baccb97a.html">Products</a>
+			<a href="Careers e50bd8e561c74102ab8e2b96276809c7.html">Careers</a>
+			<a href="Pricing 772b9e19dd8d440db9259d5150d8d0a5.html">Pricing</a>
+		</div>
+
+		<div class="menu">
+			<a href="https://app.gratheon.com/account/authenticate/">Log&nbsp;in</a>
+		</div>
+	</nav>
