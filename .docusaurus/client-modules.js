ee0d8ac7ec84e8f210c1eb26dfcff4692fdeea04
Artjom Kurapov <artkurapov@gmail.com>
2025-04-15 18:47:13 +0300

    update privacy policy

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,5 +1,4 @@
 export default [
-  require("/Users/artjom/git/website/node_modules/@docusaurus/preset-classic/node_modules/@docusaurus/plugin-google-gtag/lib/gtag"),
   require("/Users/artjom/git/website/node_modules/infima/dist/css/default/default.css"),
   require("/Users/artjom/git/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
   require("/Users/artjom/git/website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
