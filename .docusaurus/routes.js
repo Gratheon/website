0f5779f528c3bcf535a8debf811bb1c4dc5823c5
Artjom Kurapov <artkurapov@gmail.com>
2025-05-15 18:59:23 +0300

    build website on host as docusaurus needs lots of RAM

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,43 +3,8 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/__docusaurus/debug',
-    component: ComponentCreator('/__docusaurus/debug', '5ff'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/config',
-    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/content',
-    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/globalData',
-    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/metadata',
-    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/registry',
-    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/routes',
-    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
-    exact: true
-  },
-  {
     path: '/blog',
-    component: ComponentCreator('/blog', 'a6e'),
+    component: ComponentCreator('/blog', '50a'),
     exact: true
   },
   {
@@ -213,13 +178,23 @@ export default [
     exact: true
   },
   {
+    path: '/blog/2025/05/13/ ESTBAN Red flags in legal',
+    component: ComponentCreator('/blog/2025/05/13/ ESTBAN Red flags in legal', '84e'),
+    exact: true
+  },
+  {
+    path: '/blog/2025/05/15/ 🔬 gscan',
+    component: ComponentCreator('/blog/2025/05/15/ 🔬 gscan', 'bb7'),
+    exact: true
+  },
+  {
     path: '/blog/archive',
     component: ComponentCreator('/blog/archive', '182'),
     exact: true
   },
   {
     path: '/blog/page/2',
-    component: ComponentCreator('/blog/page/2', 'cb5'),
+    component: ComponentCreator('/blog/page/2', '7b9'),
     exact: true
   },
   {
