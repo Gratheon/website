accecceafbfe7b2ea9f7b70ea284c6d16143adf2
Artjom Kurapov <artkurapov@gmail.com>
2025-12-08 01:56:25 +0200

    vault backup: 2025-12-08 01:56:25

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,6 +3,41 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '5ff'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
+    exact: true
+  },
+  {
     path: '/blog',
     component: ComponentCreator('/blog', 'ca2'),
     exact: true
@@ -234,15 +269,15 @@ export default [
   },
   {
     path: '/about',
-    component: ComponentCreator('/about', 'a07'),
+    component: ComponentCreator('/about', '984'),
     routes: [
       {
         path: '/about',
-        component: ComponentCreator('/about', '126'),
+        component: ComponentCreator('/about', '870'),
         routes: [
           {
             path: '/about',
-            component: ComponentCreator('/about', '57f'),
+            component: ComponentCreator('/about', 'dc6'),
             routes: [
               {
                 path: '/about/',
@@ -631,6 +666,12 @@ export default [
               {
                 path: '/about/company/prototron_dataroom/LEGAL_ANALYSIS_MENTOR_PREP',
                 component: ComponentCreator('/about/company/prototron_dataroom/LEGAL_ANALYSIS_MENTOR_PREP', '9ee'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/about/company/prototron_dataroom/MARKETING_MENTOR_PREP',
+                component: ComponentCreator('/about/company/prototron_dataroom/MARKETING_MENTOR_PREP', 'c05'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
